d8417797d6c733b0831d9883d09b77083d601c9f
Tommy Nguyen <sixty22tommy@gmail.com>
2022-06-30 15:10:42 -0700

    commit tree 2297cdf068e9e0b31401b72d39575096db2f46bb

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (driverArray) => {
+    return driverArray.slice(0,2)
+}
+
+const returnLastTwoDrivers = (driverArray) => {
+    return driverArray.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// selectingDrivers[0](['Sally', "bob"])
+
+const createFareMultiplier = (multiplier) => {
+    return (fare) => {
+        return multiplier * fare
+    }
+}
+const fareMultiplier = (multiplier, fare) => {
+    return multiplier * fare
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (driverArray, driverSelectFunc) => {
+    return driverSelectFunc(driverArray)
+}
